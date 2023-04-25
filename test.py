3cd2132e1a5a86dcc3692c85447087c624ea5143
AnanasikDev <oleg.brovchenkov@gmail.com>
2023-04-25 12:26:45 +0300

    Saving binary dictionary is tested

--- test.py
+++ test.py
@@ -1,28 +1,28 @@
+import pickle
+import struct
 
-def determine_type(path):
-    with open(path, 'rb') as file:
-        r = file.read()
-        try:
-            data = str(r, "utf-8")
-            return "TEXT"
-        except:
-            return "BYTE"
+# Create a dictionary to encode
+my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
 
+# Encode the dictionary into a file
+with open('my_dict.pickle', 'wb') as f:
+    f.write(struct.pack('<4s', bytes.fromhex('CAFEFADE')))
 
-print(determine_type("file.txt"))
-print(determine_type("img_UNPACKED"))
-print(determine_type("img.seven"))
-print(determine_type("img.bmp"))
-print(determine_type("Text File"))
+with open('my_dict.pickle', 'ab') as f:
+    pickle.dump(my_dict, f)
 
+# Decode the dictionary from the file
+with open('my_dict.pickle', 'rb') as f:
+    # Seek to the start of the dictionary
+    f.seek(4)
+    # Read the binary data into a bytes object
+    data = f.read()
+    # Decode the binary data into a dictionary using pickle
+    decoded_dict = pickle.loads(data)
 
-"""
+with open('my_dict.pickle', 'ab') as f:
+    for i in range(100):
+        f.write(b'a0')
 
-def define_algorithm(self, file):
-    text_ = ["txt", "py", "cpp", "cs"]
-    if Path.get_file_extension(Path.get_file(file)) in text_:
-        self.algorithm = Information.TEXT
-    else:
-        self.algorithm = Information.BYTE
-
-"""
+# Print the decoded dictionary
+print(decoded_dict)
