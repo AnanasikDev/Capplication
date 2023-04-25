9c0d1d629389fc7f2b662fc56be0b3900e938ab1
AnanasikDev <oleg.brovchenkov@gmail.com>
2023-04-25 21:33:17 +0300

    Huffman realized

--- myapp/myapp/huffman.py
+++ myapp/myapp/huffman.py
@@ -1,5 +1,7 @@
 from collections import Counter
 import struct
+import pickle
+import signature
 
 class NodeTree(object):
     def __init__(self, left=None, right=None):
@@ -42,7 +44,7 @@ def make_tree(nodes):
     return nodes[0][0]
 
 
-def encode_data(encoding):
+def encode_data(encoding, data):
     encoded_data = ''.join(encoding[char] for char in data)
     return encoded_data
 
@@ -61,8 +63,6 @@ def decode_data(encoding, solidata):
         bullet += c
         if bullet in list(decoding.keys()):
             decoded.append(decoding[bullet])
-            if bullet != '1':
-                print(bullet)
             bullet = ''
 
     return decoded
@@ -78,6 +78,15 @@ def splitchunks(data):
     return a, 8 - l + c
 
 
+def inverse_bytes(seq):
+    if isinstance(seq, str):
+        l = len(seq)
+        a = [seq[i:i+2] for i in range(0, l, 2)]
+        return ''.join(a[::-1])
+    elif isinstance(seq, list):
+        return seq[::-1]
+
+
 def int2hex(i):
     return hex(i)[2::].zfill(2)
 
@@ -90,13 +99,13 @@ def int2bin(i):
 def hex2bin(h):
     return bin(int(h, 16))[2::].zfill(8)
 
+def bin2int(b):
+    return int(b, 2)
 
-if __name__ == '__main__':
 
-    # path = '/home/jam/IT/Graphs/Capplication/myapp/myapp/data/text.txt'
-    path = '/home/jam/IT/Graphs/Capplication/myapp/myapp/data/img2.bmp'
+def encode(input_file, output_file):
     data = []
-    with open(path, 'rb') as file:
+    with open(input_file, 'rb') as file:
         while True:
             chunk = file.read(1)
             if chunk == b'':
@@ -111,24 +120,58 @@ if __name__ == '__main__':
     encoding = dict(sorted(encoding.items(), key=lambda x: len(x[1])))
     for i in encoding:
         print(f'{i} : {encoding[i]}')
-    encoded = encode_data(encoding)
-
-    # decoded = decode_data({'a' : '001', 'b': '0001', 'c' : '11001'}, '001000111001')
-    # print(decoded)
-
-    # exit()
-
-    # print(bin2hex('10000000'))
+    encoded = encode_data(encoding, data)
 
     encoded, bits2ignore = splitchunks(encoded)
 
-    with open("./data/encoded", "wb") as file:
+    with open(output_file, "wb") as file:
+        encoded_dict = pickle.dumps(encoding)
+        l = len(encoded_dict)
+
+        file_size = 4 + 4 + 1 + 1 + 1 + 4 + 2*3*4 + l + len(encoded)
+
+        file.write(struct.pack('<4siBBBiiii', bytes.fromhex(signature.signature), file_size, 1, 1, bits2ignore, l, 0, 0, 0))
+        pickle.dump(encoding, file)
+        file.write(struct.pack('<iii', 0, 0, 0))
         for byte in encoded:
             if byte == b'':
                 break
             file.write(struct.pack('<1s', bytes.fromhex(bin2hex(byte))))
 
-    with open("./data/encoded", "rb") as file:
+
+def decode(input_file, output_file, bits2ignore, dict_size):
+    with open(input_file, "rb") as file:
+        # data = file.read()
+        # data = data[:len(data)-bits2ignore:]
+
+        file.seek(27)
+        d = file.read()
+        encoding = pickle.loads(d)
+
+        m = 4 + 4 + 1 + 1 + 1 + 4 + 2*3*4 + dict_size
+        file.seek(m)
+
+        data = []
+        while True:
+            chunk = file.read(1)
+            if chunk == b'':
+                break
+            data.append(int2bin(struct.unpack('<B', chunk)[0]))
+
+        data = ''.join(data)
+
+        decoded = decode_data(encoding, data)
+
+        with open(output_file, "wb") as file:
+            for byte in decoded:
+                if byte == b'':
+                    break
+                file.write(struct.pack('<1s', bytes.fromhex(byte)))
+
+
+m = 0
+def read_params(path):
+    with open(path, "rb") as file:
         data = []
         while True:
             chunk = file.read(1)
@@ -137,13 +180,35 @@ if __name__ == '__main__':
 
             p = struct.unpack('<B', chunk)[0]
             data.append(int2bin(p))
-        data = ''.join(data)
-        data = data[:len(data)-bits2ignore:]
+        # data = ''.join(data)
 
-        decoded = decode_data(encoding, data)
+        # print(data)
+        # exit()
 
-        with open("./data/decoded", "wb") as file:
-            for byte in decoded:
-                if byte == b'':
-                    break
-                file.write(struct.pack('<1s', bytes.fromhex(byte)))
+        def read_bytes(n):
+            global m
+            m += n
+            print(''.join(inverse_bytes(data[(m-n):m:])))
+            return ''.join(inverse_bytes(data[(m-n):m:]))
+
+        signature = read_bytes(4)
+        file_size = bin2int(read_bytes(4))
+        algorithm = bin2int(read_bytes(1))
+        iterations = bin2int(read_bytes(1))
+        bits2ignore = bin2int(read_bytes(1))
+        dict_size = bin2int(read_bytes(4))
+
+        print(signature, file_size, algorithm, iterations, bits2ignore, dict_size)
+
+    return signature, file_size, algorithm, iterations, bits2ignore, dict_size
+
+
+if __name__ == '__main__':
+
+    # path = '/home/jam/IT/Graphs/Capplication/myapp/myapp/data/text.txt'
+    path = '/home/jam/IT/Graphs/Capplication/myapp/myapp/data/img2.bmp'
+
+
+    # encode(path, "./output.huffman")
+    signature, file_size, algorithm, iterations, bits2ignore, dict_size = read_params("./output.huffman")
+    decode("./output.huffman", "./decoded", bits2ignore, dict_size)
--- test.py
+++ test.py
@@ -1,28 +1,39 @@
-import pickle
-import struct
+# import pickle
+# import struct
+#
+# # Create a dictionary to encode
+# my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
+#
+# # Encode the dictionary into a file
+# with open('my_dict.pickle', 'wb') as f:
+#     f.write(struct.pack('<4s', bytes.fromhex('CAFEFADE')))
+#     encoded_dict = pickle.dumps(my_dict)
+#     l = len(encoded_dict)
+#     pickle.dump(my_dict, f)
+#     print(l)
+#
+# # Decode the dictionary from the file
+# with open('my_dict.pickle', 'rb') as f:
+#     # Seek to the start of the dictionary
+#     f.seek(4)
+#     # Read the binary data into a bytes object
+#     data = f.read()
+#     # Decode the binary data into a dictionary using pickle
+#     decoded_dict = pickle.loads(data)
+#
+#     f.seek(0)
+#     d = f.read()
+#
+# # Print the decoded dictionary
+# print(decoded_dict)
 
-# Create a dictionary to encode
-my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
+def bin2hex(b):
+    return hex(int(b, 2))[2::].zfill(2)
 
-# Encode the dictionary into a file
-with open('my_dict.pickle', 'wb') as f:
-    f.write(struct.pack('<4s', bytes.fromhex('CAFEFADE')))
+def inverse_bytes(string):
+    l = len(string)
+    a = [string[i:i+2] for i in range(0, l, 2)]
+    return ''.join(a[::-1])
 
-with open('my_dict.pickle', 'ab') as f:
-    pickle.dump(my_dict, f)
-
-# Decode the dictionary from the file
-with open('my_dict.pickle', 'rb') as f:
-    # Seek to the start of the dictionary
-    f.seek(4)
-    # Read the binary data into a bytes object
-    data = f.read()
-    # Decode the binary data into a dictionary using pickle
-    decoded_dict = pickle.loads(data)
-
-with open('my_dict.pickle', 'ab') as f:
-    for i in range(100):
-        f.write(b'a0')
-
-# Print the decoded dictionary
-print(decoded_dict)
+print(int(inverse_bytes("1fed0100"), 16))
+print(int("0001ed1f"[::1], 16))
